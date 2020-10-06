2663a4ef45db132d95247e36910568f3b5610922
James Hinton <jhint2011@gmail.com>
2020-10-05 22:23:44 -0400

    change names

--- src/main/java/com/javajames/awsimageupload/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/javajames/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -13,8 +13,8 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.fromString("7aee0f60-3983-4b05-acef-0ae6617e67ac"), "janetjones", null));
-        USER_PROFILES.add(new UserProfile(UUID.fromString("f4e0630f-e0ee-4c6e-bc32-b50c5f345c70"), "antoniojunior", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("7aee0f60-3983-4b05-acef-0ae6617e67ac"), "taylordiaz", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("f4e0630f-e0ee-4c6e-bc32-b50c5f345c70"), "markmcgee", null));
     }
 
     public List<UserProfile> getUserProfiles(){
